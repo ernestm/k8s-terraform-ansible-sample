3bd1e53017cac80013405ec61e82f61b54f74ade
Ernest Mueller <ernest.mueller@gmail.com>
2022-11-06 00:09:20 +0000

    Update terraform code to work with 1.13

--- terraform/etcf.tf
+++ terraform/etcf.tf
@@ -3,23 +3,23 @@
 #########################
 
 resource "aws_instance" "etcd" {
-    count = 3
-    ami = "${lookup(var.amis, var.region)}"
-    instance_type = "${var.etcd_instance_type}"
+  count         = 3
+  ami           = lookup(var.amis, var.region)
+  instance_type = var.etcd_instance_type
 
-    subnet_id = "${aws_subnet.kubernetes.id}"
-    private_ip = "${cidrhost(var.vpc_cidr, 10 + count.index)}"
-    associate_public_ip_address = true # Instances have public, dynamic IP
+  subnet_id                   = aws_subnet.kubernetes.id
+  private_ip                  = cidrhost(var.vpc_cidr, 10 + count.index)
+  associate_public_ip_address = true # Instances have public, dynamic IP
 
-    availability_zone = "${var.zone}"
-    vpc_security_group_ids = ["${aws_security_group.kubernetes.id}"]
-    key_name = "${var.default_keypair_name}"
+  availability_zone      = var.zone
+  vpc_security_group_ids = ["${aws_security_group.kubernetes.id}"]
+  key_name               = var.default_keypair_name
 
-    tags {
-      Owner = "${var.owner}"
-      Name = "etcd-${count.index}"
-      ansibleFilter = "${var.ansibleFilter}"
-      ansibleNodeType = "etcd"
-      ansibleNodeName = "etcd${count.index}"
-    }
+  tags = {
+    Owner           = var.owner
+    Name            = "etcd-${count.index}"
+    ansibleFilter   = var.ansibleFilter
+    ansibleNodeType = "etcd"
+    ansibleNodeName = "etcd${count.index}"
+  }
 }
